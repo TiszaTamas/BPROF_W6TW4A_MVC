84c75f23a3207fa99abb17c6b229f932e64bbc7d
TiszaTamas <tisza.tamas01@gmail.com>
2021-01-05 12:13:32 +0100

    Added Web component to solution.

---
+++ C#/AdventurersGuild/AdventurersGuild.Repository/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventurersGuild.Repository
+{
+    interface IRepository<T> where T : new()
+    {
+        List<T> GetPeople();
+
+        List<T> GetMonsters();
+
+        List<T> GetAdventurers();
+
+        List<T> GetJobs();
+
+        List<T> GetClients();
+
+        List<T> GetAll();
+
+        void Add(T item);
+
+        void Delete(T item);
+
+        void Delete(string uid);
+
+        T Read(string uid);
+
+        IQueryable<T> Read();
+
+        void Update(string oldid, T newitem);
+
+        void Save();
+
+    }
+}
